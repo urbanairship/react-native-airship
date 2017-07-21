395e35632d06faa7aebbeb945b1e2f17c86dfa97
Gatlin Hebert <gatlinhebert@users.noreply.github.com>
2017-07-21 15:45:39 -0700

    [MB-2245] Log warning if the version of AirshipKit is below a recommended version. (#24)
    * Log warning if the version of AirshipKit is below a recommended version.
    
    * Update version to 8.4.3

--- ios/UARCTModule/UARCTAutopilot.h
+++ ios/UARCTModule/UARCTAutopilot.h
@@ -3,6 +3,7 @@
 #import <Foundation/Foundation.h>
 
 extern NSString *const UARCTPresentationOptionsStorageKey;
+extern NSString *const UARCTAirshipKitRecommendedVersion;
 
 /**
  * Handles takeOff for the Urban Airship SDK.
