a8284441cc911585db2a3edc1a7e7a1051652a33
Brian Batchelder <BrianBatchelder@users.noreply.github.com>
2019-10-02 11:15:28 -0700

    [MOBILE-833] ReactNative: Add iOS notification service extension back into sample (#220)

---
+++ example/ios/ServiceExtension/NotificationService.h
@@ -0,0 +1,8 @@
+/* Copyright 2017 Urban Airship and Contributors */
+
+// NotificationService.h
+@import AirshipAppExtensions;
+
+@interface NotificationService : UAMediaAttachmentExtension
+
+@end
---
+++ example/ios/ServiceExtension/NotificationService.m
@@ -0,0 +1,10 @@
+/* Copyright 2017 Urban Airship and Contributors */
+
+// NotificationService.m
+@import Foundation;
+
+#import "NotificationService.h" 
+
+@implementation NotificationService
+
+@end
