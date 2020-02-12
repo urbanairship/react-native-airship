9d95e8ccd5d10e7bb09aacd9243f1034426f320f
Ulrich Giberné <ulrich.giberne@airship.com>
2020-02-12 15:37:15 +0100

    Use latest iPhone. Use sudo for npm install

--- scripts/run_ci_tasks.sh
+++ scripts/run_ci_tasks.sh
@@ -60,7 +60,7 @@ if $IOS; then
     PROJECT_PLATFORM_PATH="$(pwd)"
     DERIVED_DATA=$(mktemp -d /tmp/ci-derived-data-XXXXX)
     TARGET_SDK='iphonesimulator'
-    TEST_DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 11 Pro'
+    TEST_DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 11 Pro Max'
 
     # Install the pods
     pod install
