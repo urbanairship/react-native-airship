aab6d1c65659d8f948644ab9aebeb6eaa7ba2a64
Antoine PERY-KASZA <antoine.pery-kasza@airship.com>
2020-02-18 10:08:25 -0800

    [MOBILE-1270] Add number attribute support to React Native (#248)
    * Adding number support to React Native Attributes
    
    * Variable fixes
    
    * [MOBILE-1270] Add iOS support
    
    * Fixes and swap key/value order for setAttribute method
    
    Co-authored-by: Ryan Lepinski <ryan.lepinski@urbanairship.com>

--- js/AttributeEditor.js
+++ js/AttributeEditor.js
@@ -21,7 +21,7 @@ class AttributeEditor {
    * @param {string} name The attribute name.
    * @return {AttributeEditor} The attribute editor instance.
    */
-  setAttribute(value: string, name: string): AttributeEditor {
+  setAttribute(name: string, value: string|number): AttributeEditor {
     var operation = { "action": "set", "value": value, "key": name }
     this.operations.push(operation)
     return this;
