47a7c5bb5579d409addf1f1ba66689feb550d3cb
Mouna Khemiri <mouna.khemiri@airship.com>
2020-05-07 16:06:54 +0200

    Update SDK iOS TO 13.2.1

--- src/ios/UARCTModule/UARCTAutopilot.m
+++ src/ios/UARCTModule/UARCTAutopilot.m
@@ -7,7 +7,7 @@
 #import "UARCTModuleVersion.h"
 
 NSString *const UARCTPresentationOptionsStorageKey = @"com.urbanairship.presentation_options";
-NSString *const UARCTAirshipRecommendedVersion = @"13.1.1";
+NSString *const UARCTAirshipRecommendedVersion = @"13.2.1";
 
 @implementation UARCTAutopilot
 
