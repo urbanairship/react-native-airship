21540c87c8763369b96886daa2a5cfa9dd6b63d7
Mouna Khemiri <mouna.khemiri@airship.com>
2020-05-15 14:17:50 +0200

    Fix conflit issue

--- src/ios/UARCTModule/UrbanAirshipReactModule.m
+++ src/ios/UARCTModule/UrbanAirshipReactModule.m
@@ -574,33 +574,27 @@ RCT_REMAP_METHOD(getActiveNotifications,
     }];
 }
 
+#pragma mark -
 #pragma mark Helper methods
 
 - (UAAttributeMutations *)mutationsWithOperations:(NSArray *)operations {
     UAAttributeMutations *mutations = [UAAttributeMutations mutations];
 
     for (NSDictionary *operation in operations) {
-            NSString *action = operation[@"action"];
-            NSString *name = operation[@"key"];
-            id value = operation[@"value"];
+        NSString *action = operation[@"action"];
+        NSString *name = operation[@"key"];
+        id value = operation[@"value"];
 
-            if ([action isEqualToString:@"set"]) {
-                NSString *valueType = operation[@"type"];
-                if ([valueType isEqualToString:@"string"]) {
-                    [mutations setString:value forAttribute:name];
-                } else if ([valueType isEqualToString:@"number"]) {
-                    [mutations setNumber:value forAttribute:name];
-                } else if ([valueType isEqualToString:@"date"]) {
-                    // JavaScript's date type doesn't pass through the JS to native bridge. Dates are instead serialized as milliseconds since epoch.
-                    NSDate *date = [NSDate dateWithTimeIntervalSince1970:[(NSNumber *)value doubleValue] / 1000.0];
-                    [mutations setDate:date forAttribute:name];
-                } else {
-                    UA_LWARN("Unknown channel attribute type: %@", valueType);
-                }
-            } else if ([action isEqualToString:@"remove"]) {
-                [mutations removeAttribute:name];
+        if ([action isEqualToString:@"set"]) {
+            if ([value isKindOfClass:[NSString class]]) {
+                [mutations setString:value forAttribute:name];
+            } else if ([value isKindOfClass:[NSNumber class]]) {
+                [mutations setNumber:value forAttribute:name];
             }
+        } else if ([action isEqualToString:@"remove"]) {
+            [mutations removeAttribute:name];
         }
+    }
     return mutations;
 }
 
