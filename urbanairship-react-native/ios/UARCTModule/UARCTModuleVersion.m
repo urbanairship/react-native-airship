ad9fde6db2b80633b0219d48233c408327a058c1
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2020-09-21 08:20:42 -0700

    Release 9.0.0 (#297)

--- urbanairship-react-native/ios/UARCTModule/UARCTModuleVersion.m
+++ urbanairship-react-native/ios/UARCTModule/UARCTModuleVersion.m
@@ -4,7 +4,7 @@
 
 @implementation UARCTModuleVersion
 
-NSString *const airshipModuleVersionString = @"8.1.0";
+NSString *const airshipModuleVersionString = @"9.0.0";
 
 + (nonnull NSString *)get {
     return airshipModuleVersionString;
