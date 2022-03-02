3b31ee06e2f5c915082c1fcde685896e9c4742aa
Josh Yaganeh <319444+jyaganeh@users.noreply.github.com>
2022-03-02 13:46:09 -0800

    Update pref center getConfiguration() to return a JsonValue

--- urbanairship-preference-center-react-native/js/AirshipPreferenceCenter.ts
+++ urbanairship-preference-center-react-native/js/AirshipPreferenceCenter.ts
@@ -3,7 +3,7 @@
 'use strict';
 
 import { NativeModules } from 'react-native';
-import { UrbanAirship } from 'urbanairship-react-native'
+import {  UrbanAirship, JsonValue } from 'urbanairship-react-native'
 import { UAEventEmitter } from 'urbanairship-react-native/js/UAEventEmitter'
 
 /**
@@ -16,86 +16,6 @@ const UrbanAirshipModule = NativeModules.UrbanAirshipReactModule;
  * @hidden
  */
 const EventEmitter = new UAEventEmitter();
-
- /**
-  *Preference section item.
-  */
- export interface Item {
-
-  /**
-   * The item identifier.
-   */
-  id: String;
-
-  /**
-   * Optional display info.
-   */
-  display: CommonDisplay;
-
-  /**
-  * The subscription identifier.
-  */
-  subscriptionId: String;
-
- }
-
- /**
-  *Common display info.
-  */
- export interface CommonDisplay {
-
-  /**
-   *The optional name/title.
-   */
-  name?: String;
-
-  /**
-   *The optional description/subtitle.
-   */
-  description?: String;
-
-  }
-
- /**
-  *Preference section.
-  */
- export interface Section {
-
-  /**
-   * Section identifier.
-   */
-   id: String;
-
-  /**
-   * Section items.
-   */
-   items: Item[];
-
-   /**
-   * Optional display info.
-   */
-   display: CommonDisplay;
- }
-
- /**
- * Preference center config
- */
- export interface PreferenceCenterConfig {
-  /**
-   * The config identifier.
-   */
-  id: string;
-
-  /**
-   * The preference center sections.
-   */
-  sections: Section[];
-
-  /**
-   * Optional common display info.
-   */
-  display: CommonDisplay;
- }
 
 /**
  * A listener subscription.
@@ -122,18 +42,18 @@ export class AirshipPreferenceCenter {
     AirshipPreferenceCenterModule.open(preferenceCenterId);
   }
 
-  static getConfiguration(preferenceCenterId: String): Promise<PreferenceCenterConfig> {
-      return AirshipPreferenceCenterModule.getConfiguration(preferenceCenterId);
+  static getConfiguration(preferenceCenterId: String): Promise<JsonValue> {
+    return AirshipPreferenceCenterModule.getConfiguration(preferenceCenterId);
   }
 
-  static setUseCustomPreferenceCenterUi(useCustomUi: boolean, preferenceCenterId:String) {
+  static setUseCustomPreferenceCenterUi(useCustomUi: boolean, preferenceCenterId: String) {
     AirshipPreferenceCenterModule.setUseCustomPreferenceCenterUi(useCustomUi, preferenceCenterId);
   }
 
   static addPreferenceCenterOpenListener(listener: (...args: any[]) => any): Subscription {
     EventEmitter.addListener("com.urbanairship.open_preference_center", listener);
-      return new Subscription(() => {
-        EventEmitter.removeListener("com.urbanairship.open_preference_center", listener);
-      });
-    }
+    return new Subscription(() => {
+      EventEmitter.removeListener("com.urbanairship.open_preference_center", listener);
+    });
+  }
 }
