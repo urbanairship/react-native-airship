635a12e636b3ed3ff1f5270c7ed99f7a523ec366
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2022-03-16 11:30:38 -0700

    Release 14.1.0 (#411)

--- urbanairship-accengage-react-native/ios/AirshipAccengageReactModuleVersion.m
+++ urbanairship-accengage-react-native/ios/AirshipAccengageReactModuleVersion.m
@@ -2,7 +2,7 @@
 
 #import "AirshipAccengageReactModuleVersion.h"
 
-NSString *const moduleVersionString = @"14.0.0";
+NSString *const moduleVersionString = @"14.1.0";
 
 @implementation AirshipAccengageReactModuleVersion
 
