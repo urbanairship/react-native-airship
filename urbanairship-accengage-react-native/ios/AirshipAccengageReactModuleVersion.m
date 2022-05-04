c041ea9566903bf1a8b31803cbeab7846030c993
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2022-05-04 15:06:20 -0700

    Update to latest SDKs

--- urbanairship-accengage-react-native/ios/AirshipAccengageReactModuleVersion.m
+++ urbanairship-accengage-react-native/ios/AirshipAccengageReactModuleVersion.m
@@ -2,7 +2,7 @@
 
 #import "AirshipAccengageReactModuleVersion.h"
 
-NSString *const moduleVersionString = @"14.2.1";
+NSString *const moduleVersionString = @"14.3.0";
 
 @implementation AirshipAccengageReactModuleVersion
 
