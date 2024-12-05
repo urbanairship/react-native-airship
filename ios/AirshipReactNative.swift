634ad74dc13fb84a345308b58f36e5ab8a71b58e
crow <david.crow@airship.com>
2024-12-05 15:55:40 -0800

    Update version

--- ios/AirshipReactNative.swift
+++ ios/AirshipReactNative.swift
@@ -39,7 +39,7 @@ public class AirshipReactNative: NSObject {
         AirshipProxy.shared
     }
 
-    public static let version: String = "20.0.4"
+    public static let version: String = "20.1.0"
 
     private let eventNotifier = EventNotifier()
 
