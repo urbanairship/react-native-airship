04d6bcd022ecf4259579d50958973a67dafe1db3
Ryan Lepinski <ryan.lepinski@airship.com>
2025-05-23 14:15:03 -0700

    Release 21.7.0

--- ios/AirshipReactNative.swift
+++ ios/AirshipReactNative.swift
@@ -39,7 +39,7 @@ public class AirshipReactNative: NSObject {
         AirshipProxy.shared
     }
 
-    public static let version: String = "21.6.0"
+    public static let version: String = "21.7.0"
 
     private let eventNotifier = EventNotifier()
 
