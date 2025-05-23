a42d699bd72d385c2e8b0acb49336ce03b03b160
Ryan Lepinski <ryan.lepinski@airship.com>
2025-05-23 14:42:08 -0700

    Release 24.3.0 (#661)
    * Release 24.3.0
    
    * Update CHANGELOG.md
    
    * Remove Pod lockfile for example

--- ios/AirshipReactNative.swift
+++ ios/AirshipReactNative.swift
@@ -39,7 +39,7 @@ public class AirshipReactNative: NSObject {
         AirshipProxy.shared
     }
 
-    public static let version: String = "24.2.0"
+    public static let version: String = "24.3.0"
 
     private let eventNotifier = EventNotifier()
 
