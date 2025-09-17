67e95e89ddae9929b1d8cac57cc5314f76f6b3fc
crow <david.crow@airship.com>
2025-09-16 18:11:47 -0700

    Release 24.7.0

--- ios/AirshipReactNative.swift
+++ ios/AirshipReactNative.swift
@@ -39,7 +39,7 @@ public class AirshipReactNative: NSObject {
         AirshipProxy.shared
     }
 
-    public static let version: String = "24.6.0"
+    public static let version: String = "24.7.0"
 
     private let eventNotifier = EventNotifier()
 
